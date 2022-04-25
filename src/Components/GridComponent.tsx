572f5262e8588fc1d4d55129cafa26001e4de206
MuriloM <muriloommaia@gmail.com>
2022-04-25 18:44:53 -0300

    feat: cria estrutura de grid

---
+++ src/Components/GridComponent.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default function GridComponent() {
+  return (
+    <div className="w-11/12">
+      <div className="grid grid-cols-3 gap-4 ">
+        <div className="">
+          Box 1
+        </div>
+        <div>
+          Box 1
+        </div>
+        <div>
+          Box 1
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import GridComponent from '../Components/GridComponent';
 import TitleComponent from '../Components/TitleComponent';
 
 export default function Home() {
   return (
-    <div className="flex justify-center">
+    <div className="flex flex-col items-center">
       <nav>
         <TitleComponent />
       </nav>
+      <main className="w-full flex justify-center">
+        <GridComponent />
+      </main>
     </div>
   );
 }
