5a255076cf5dc39914e721fd27214e06a07ae9fa
MuriloM <muriloommaia@gmail.com>
2022-04-25 17:28:38 -0300

    chore: configure eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="flex justify-center">
-    <h1 className="font-bold text-lg text-blue-900">
-      React and Tailwind with Vitejs!
-    </h1>
-  </div>
-  )
+      <h1 className="font-bold text-lg text-blue-900">
+        React and Tailwind with Vitejs!
+      </h1>
+
+    </div>
+  );
 }
 
-export default App
+export default App;
