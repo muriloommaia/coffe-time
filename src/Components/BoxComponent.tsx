8859da70ad1740138920b5d060b453f36b70ac12
MuriloM <muriloommaia@gmail.com>
2022-04-25 19:50:10 -0300

    feat: implementa box component

---
+++ src/Components/BoxComponent.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../store';
+
+export default function BoxComponent({ text, image }: { text: 'lectura' | 'escrita' | 'compartir'; image: string }) {
+  const { time, timeLeft, active } = useSelector((state: RootState) => state[text]);
+  const secondsToMinutes = (seconds: number) => {
+    const minutes = Math.floor(seconds / 60);
+    const secondsLeft = seconds % 60;
+    return `${minutes}:${secondsLeft < 10 ? `0${secondsLeft}` : secondsLeft}`;
+  };
+  return (
+    <div className={`flex flex-col items-center cursor-pointer border-2 shadow-2xl hover:scale-105 ${active ? 'border-green-800' : 'border-black'}`}>
+      <div>
+        {text.toUpperCase()}
+      </div>
+      <div className="w-10/12 m-auto p-2">
+        <img src={image} alt={text} className="w-5/12 m-auto" />
+      </div>
+      <div>
+        {secondsToMinutes(timeLeft)}
+      </div>
+    </div>
+  );
+}
--- src/Components/GridComponent.tsx
+++ src/Components/GridComponent.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
+import book from '../images/book.svg';
+import talk from '../images/talk.svg';
+import write from '../images/write.svg';
+import { BoxType } from '../types/BoxType';
+import BoxComponent from './BoxComponent';
 
 export default function GridComponent() {
+  const grid:BoxType[] = [
+    ['lectura', book],
+    ['escrita', write],
+    ['compartir', talk],
+  ];
   return (
     <div className="w-11/12">
       <div className="grid grid-cols-3 gap-4 ">
-        <div className="">
-          Box 1
-        </div>
-        <div>
-          Box 1
-        </div>
-        <div>
-          Box 1
-        </div>
+        {grid.map(([text, image]) => (
+          <BoxComponent text={text} image={image} />
+        ))}
       </div>
     </div>
   );
