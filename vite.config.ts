ca58348e7b01ac2445b2b5087ff29d57e6bc8856
MuriloM <muriloommaia@gmail.com>
2022-04-28 16:05:36 -0300

    feat: altera titulo do documento

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+/* eslint-disable import/no-extraneous-dependencies */
+// eslint-disable-next-line import/no-extraneous-dependencies
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
